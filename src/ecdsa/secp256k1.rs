67770181ce74e25f6bb2dde402ea115741f455c9
Shella Stephens <shella@iqlusion.io>
2020-09-22 15:37:37 -0700

    Bump k256 to v0.5.1 & cargo build --all-features

--- src/ecdsa/secp256k1.rs
+++ src/ecdsa/secp256k1.rs
@@ -3,7 +3,7 @@
 #[cfg(feature = "test-vectors")]
 pub mod test_vectors;
 
-pub use k256::{PublicKey, Secp256k1, SecretKey};
+pub use k256::Secp256k1;
 
 /// secp256k1 ASN.1 signature
 pub type Asn1Signature = super::Asn1Signature<Secp256k1>;
