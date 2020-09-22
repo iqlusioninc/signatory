f36c031774d09607cd95717f102249e25e8dcce0
Shella Stephens <shella@iqlusion.io>
2020-09-22 15:41:00 -0700

    Bump p384 to v0.4.0 & cargo build --release --all-features

--- src/ecdsa/nistp384.rs
+++ src/ecdsa/nistp384.rs
@@ -3,7 +3,7 @@
 #[cfg(feature = "test-vectors")]
 pub mod test_vectors;
 
-pub use p384::{NistP384, PublicKey, SecretKey};
+pub use p384::NistP384;
 
 /// NIST P-384 ASN.1 signature
 pub type Asn1Signature = super::Asn1Signature<NistP384>;
