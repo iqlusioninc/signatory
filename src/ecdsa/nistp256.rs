453267f58b036011fe3b3d89ae4671eba55994b5
Tony Arcieri <tony@iqlusion.io>
2020-10-08 15:21:02 -0700

    Fix breakages in the various providers; MSRV 1.44+
    Also bumps the `secp256k1` crate dependency to v0.19

--- src/ecdsa/nistp256.rs
+++ src/ecdsa/nistp256.rs
@@ -5,6 +5,12 @@ pub mod test_vectors;
 
 pub use p256::NistP256;
 
+/// NIST P-256 public key
+pub type PublicKey = super::PublicKey<NistP256>;
+
+/// NIST P-256 secret key
+pub type SecretKey = super::SecretKey<NistP256>;
+
 /// NIST P-256 ASN.1 signature
 pub type Asn1Signature = super::Asn1Signature<NistP256>;
 
--- src/ecdsa/secp256k1.rs
+++ src/ecdsa/secp256k1.rs
@@ -5,6 +5,12 @@ pub mod test_vectors;
 
 pub use k256::Secp256k1;
 
+/// secp256k1 public key
+pub type PublicKey = super::PublicKey<Secp256k1>;
+
+/// secp256k1 secret key
+pub type SecretKey = super::SecretKey<Secp256k1>;
+
 /// secp256k1 ASN.1 signature
 pub type Asn1Signature = super::Asn1Signature<Secp256k1>;
 
