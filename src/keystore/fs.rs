c848f28533ef52ccdaccafb027063420ec8946b4
Tony Arcieri <tony@iqlusion.io>
2021-05-19 10:28:00 -0700

    Rewrite as signature KeyRing/KeyStore library
    Closes #71, which notes that a lot of the previous functionality of this
    library has been extracted into RustCrypto libs or incorporated into the
    crates it used to provide wrappers for, including `ed25519-dalek`. The
    issue enquires as to what the future of Signatory should be.
    
    This commit completely replaces the existing codebase with the
    groundwork for a much higher level signature library based around the
    following concepts:
    
    - `KeyRing`: provides a data structure for storing multiple signature
      keys while also spanning multiple signature algorithms
    - `KeyStore`: (as a prospective trait) provides storage for keys which
      can be added to a `KeyRing` prior to use
    
    The initial `KeyStore` is a filesystem-backed `FsKeyStore` which manages
    PKCS#8-serialized keys. PKCS#8 is a commonly used standard for private
    key storage, and includes support for PKCS#5 password-encrypted keys.
    
    This commit adds initial support for ECDSA/secp256k1 as well as an
    initial filesystem-backed keystore which can store and load PKCS#8 keys.

---
+++ src/keystore/fs.rs
@@ -0,0 +1,100 @@
+//! Filesystem-backed keystore
+
+use crate::Result;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+#[cfg(unix)]
+use std::{fs::Permissions, os::unix::fs::PermissionsExt};
+
+/// Key labels.
+// TODO(tarcieri): extract this into a proper type that validates its contents
+pub type Label = str;
+
+/// Required filesystem mode for keystore directories (Unix-only)
+#[cfg(unix)]
+const REQUIRED_DIR_MODE: u32 = 0o700;
+
+/// Filesystem-backed keystore.
+pub struct FsKeyStore {
+    path: PathBuf,
+}
+
+impl FsKeyStore {
+    /// Create a filesystem-backed keystore at the given path, creating a new
+    /// directory and setting its file permissions.
+    pub fn create(dir_path: &Path) -> Result<Self> {
+        // TODO(tarcieri): use `create_dir_all`?
+        fs::create_dir(&dir_path)?;
+
+        #[cfg(unix)]
+        fs::set_permissions(&dir_path, Permissions::from_mode(REQUIRED_DIR_MODE))?;
+
+        Self::open(&dir_path)
+    }
+
+    /// Initialize filesystem-backed keystore, opening the directory at the
+    /// provided path and checking that it has the correct filesystem
+    /// permissions.
+    pub fn open(dir_path: &Path) -> Result<Self> {
+        let path = dir_path.canonicalize()?;
+        let st = path.metadata()?;
+
+        if !st.is_dir() {
+            todo!("return error for not a directory");
+        }
+
+        #[cfg(unix)]
+        if st.permissions().mode() & 0o777 != REQUIRED_DIR_MODE {
+            todo!("return permissions error");
+        }
+
+        Ok(Self { path })
+    }
+
+    /// Import a PKCS#8 key into the keystore.
+    pub fn store(&self, label: &Label, der: &pkcs8::PrivateKeyDocument) -> Result<()> {
+        der.write_pem_file(&self.key_path(label))?;
+        Ok(())
+    }
+
+    /// Load a PKCS#8 key from the keystore.
+    pub fn load(&self, label: &Label) -> Result<pkcs8::PrivateKeyDocument> {
+        Ok(pkcs8::PrivateKeyDocument::read_pem_file(
+            &self.key_path(label),
+        )?)
+    }
+
+    /// Compute the path for a key with a given label.
+    fn key_path(&self, label: &Label) -> PathBuf {
+        // TODO(tarcieri): extract `Label` type and validate label
+        let mut path = self.path.join(label);
+        path.set_extension("pem");
+        path
+    }
+}
+
+#[cfg(test)]
+#[allow(unused_imports)] // TODO(tarcieri): always use imports
+mod tests {
+    use super::FsKeyStore;
+    use crate::keystore::GeneratePkcs8;
+
+    #[cfg(feature = "secp256k1")]
+    #[test]
+    fn import_key() {
+        use crate::ecdsa::secp256k1;
+
+        let dir = tempfile::tempdir().unwrap();
+        let keystore = FsKeyStore::create(&dir.path().join("keys")).unwrap();
+
+        let label = "example_key";
+        let example_key = secp256k1::SigningKey::generate_pkcs8();
+        keystore.store(label, &example_key).unwrap();
+
+        let example_key2 = keystore.load(label).unwrap();
+        assert_eq!(example_key.as_ref(), example_key2.as_ref());
+    }
+}
